5c75d73ab6557ce8bb4d69fe9555bef37179367f
Diego Artiles <diego@dartiles.dev>
2025-05-10 18:57:56 -0300

    feat: add ThoughtBox component for user input
    - Introduced ThoughtBox component to capture user thoughts with a textarea.
    - Included buttons for code and image insertion, along with a tooltip for each.
    - Added a chip to indicate Markdown support.
    - Implemented accessibility features such as aria-labels and proper class names.

---
+++ src/components/thought-box.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button, Chip, Textarea, Tooltip } from "@heroui/react";
+import { Icon } from "@iconify/react";
+
+interface ThoughtBoxProps {
+  maxLength?: number;
+  placeholder?: string;
+  className?: string;
+  onSubmit?: (thought: string) => void;
+}
+
+export function ThoughtBox({ placeholder = "What's on your mind?", className }: ThoughtBoxProps) {
+  return (
+    <div className={cn("space-y-2", className)}>
+      <div className='relative'>
+        <Textarea
+          id='thought-input'
+          placeholder={placeholder}
+          aria-label='Share your thoughts'
+          aria-describedby='char-count'
+          classNames={{
+            input: "pb-10 focus-visible:outline-none",
+            inputWrapper: "p-4",
+          }}
+          rows={1}
+        />
+        <div className='absolute bottom-2 w-full px-2 rounded-medium z-10 format-popup animate-in fade-in slide-in-from-bottom-2 duration-200'>
+          <div className='flex justify-between items-center'>
+            <div className='flex items-center gap-2'>
+              <Tooltip content='Code'>
+                <Button isIconOnly size='sm' aria-label='Code text' variant='light'>
+                  <Icon icon='lucide:code' className='text-medium' />
+                </Button>
+              </Tooltip>
+              <Tooltip content='Image'>
+                <Button isIconOnly size='sm' aria-label='Code text' variant='light'>
+                  <Icon icon='lucide:image' className='text-medium' />
+                </Button>
+              </Tooltip>
+              <Chip
+                variant='bordered'
+                classNames={{
+                  content: "flex flex-row items-center gap-2 text-muted-foreground",
+                }}
+              >
+                <Icon icon='mdi:markdown' className='text-medium' />
+                Markdown supported
+              </Chip>
+            </div>
+            <Button variant='solid' color='primary'>
+              Post
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
