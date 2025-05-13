6c73ef71ecdb662359d8267e1c5ac68373c3af0b
Diego Artiles <diego@dartiles.dev>
2025-05-12 22:07:58 -0300

    feat: add createUnauthenticatedSupabaseClient function
    - Introduced createUnauthenticatedSupabaseClient to facilitate the creation of a Supabase client without authentication, enhancing flexibility for client-side operations.
    - Utilized environment variables for Supabase URL and anon key to ensure secure configuration.

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -12,3 +12,10 @@ export function createServerSupabaseClient() {
     }
   );
 }
+
+export function createUnauthenticatedSupabaseClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
