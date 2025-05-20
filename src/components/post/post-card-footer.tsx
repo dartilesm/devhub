d2f3701e60c77484640da21f33a728a871fdbdad
Diego Artiles <diego@dartiles.dev>
2025-05-20 20:50:26 -0300

    feat: enhance post footer and card components with modal state management
    - Updated PostFooter and PostCard components to utilize isModal from context for improved modal state handling.
    - Refactored reaction tooltip rendering logic to conditionally display based on modal state.
    - Cleaned up event handling in PostCard to prevent navigation when modal is active, enhancing user experience.

--- src/components/post/post-card-footer.tsx
+++ src/components/post/post-card-footer.tsx
@@ -22,7 +22,7 @@ const reactions: ReactionType[] = [
 ];
 
 export function PostFooter() {
-  const { isThreadPagePost, togglePostModal, post } = usePostContext();
+  const { isThreadPagePost, togglePostModal, post, isModal } = usePostContext();
   const [selectedReaction, setSelectedReaction] = useState<Reaction | null>(null);
   const [isReactionsTooltipOpen, setIsReactionsTooltipOpen] = useState(false);
 
@@ -43,6 +43,8 @@ export function PostFooter() {
     });
   }
 
+  console.log({ isModal });
+
   return (
     <>
       <CardFooter
@@ -50,32 +52,34 @@ export function PostFooter() {
           "px-8.5": isThreadPagePost,
         })}
       >
-        {/* Reactions */}
-        <Tooltip
-          className='relative mt-4 flex flex-row gap-2 rounded-full p-1'
-          placement='top-start'
-          content={reactions.map((reaction) => (
+        {!isModal && (
+          <>
+            {/* Reactions */}
             <Tooltip
-              key={reaction.type}
-              className='group relative'
-              content={reaction.label}
-              onOpenChange={(open) => {
-                if (open) {
-                  setIsReactionsTooltipOpen(true);
-                }
-              }}
-            >
-              <Button
-                variant='light'
-                size='sm'
-                className='rounded-full p-2 hover:scale-200 transition-all duration-300'
-                isIconOnly
-                onPress={() => handleReaction(reaction.type)}
-              >
-                <span className='text-xl'>{reaction.icon}</span>
-              </Button>
+              className='relative mt-4 flex flex-row gap-2 rounded-full p-1'
+              placement='top-start'
+              content={reactions.map((reaction) => (
+                <Tooltip
+                  key={reaction.type}
+                  className='group relative'
+                  content={reaction.label}
+                  onOpenChange={(open) => {
+                    if (open) {
+                      setIsReactionsTooltipOpen(true);
+                    }
+                  }}
+                >
+                  <Button
+                    variant='light'
+                    size='sm'
+                    className='rounded-full p-2 hover:scale-200 transition-all duration-300'
+                    isIconOnly
+                    onPress={() => handleReaction(reaction.type)}
+                  >
+                    <span className='text-xl'>{reaction.icon}</span>
+                  </Button>
 
-              {/* <span
+                  {/* <span
             className={cn(
               "absolute -bottom-8 left-1/2 hidden -translate-x-1/2 rounded-md bg-popover px-2 py-1",
               "text-xs font-medium text-popover-foreground group-hover:block"
@@ -83,67 +87,69 @@ export function PostFooter() {
           >
             {reaction.label}
           </span> */}
+                </Tooltip>
+              ))}
+              isOpen={isReactionsTooltipOpen}
+              onOpenChange={setIsReactionsTooltipOpen}
+            >
+              <Button
+                variant={!selectedReaction ? "light" : "faded"}
+                color={!selectedReaction ? "default" : "primary"}
+                size='sm'
+                className='group flex items-center gap-1 rounded-full text-gray-400 hover:text-default-foreground'
+              >
+                {selectedReaction ? (
+                  <span className='text-lg'>
+                    {reactions.find((r) => r.type === selectedReaction)?.icon}
+                  </span>
+                ) : (
+                  <StarIcon className='text-inherit' size={18} />
+                )}
+                {selectedReaction && <span className='text-sm capitalize'>{selectedReaction}</span>}
+              </Button>
             </Tooltip>
-          ))}
-          isOpen={isReactionsTooltipOpen}
-          onOpenChange={setIsReactionsTooltipOpen}
-        >
-          <Button
-            variant={!selectedReaction ? "light" : "faded"}
-            color={!selectedReaction ? "default" : "primary"}
-            size='sm'
-            className='group flex items-center gap-1 rounded-full text-gray-400 hover:text-default-foreground'
-          >
-            {selectedReaction ? (
-              <span className='text-lg'>
-                {reactions.find((r) => r.type === selectedReaction)?.icon}
-              </span>
-            ) : (
-              <StarIcon className='text-inherit' size={18} />
-            )}
-            {selectedReaction && <span className='text-sm capitalize'>{selectedReaction}</span>}
-          </Button>
-        </Tooltip>
-        <Tooltip content='Comment'>
-          <Button
-            variant='light'
-            size='sm'
-            className='rounded-full flex flex-row gap-2 text-gray-400 hover:text-default-foreground'
-            onPress={() => togglePostModal(true)}
-          >
-            <MessageSquareIcon className='text-inherit' size={18} />
-            {post?.reply_count && post?.reply_count > 0 && (
-              <span className='text-sm'>{post?.reply_count}</span>
-            )}
-          </Button>
-        </Tooltip>
-        <Tooltip content='Clone (repost)'>
-          <Button
-            variant='light'
-            size='sm'
-            className='rounded-full text-gray-400 hover:text-default-foreground'
-          >
-            <Repeat2Icon className='text-inherit' size={22} strokeWidth={1.5} />
-          </Button>
-        </Tooltip>
-        <Tooltip content='Backup (Bookmark)'>
-          <Button
-            variant='light'
-            size='sm'
-            className='rounded-full text-gray-400 hover:text-default-foreground'
-          >
-            <ArchiveIcon className='text-inherit' size={18} />
-          </Button>
-        </Tooltip>
-        <Tooltip content='More'>
-          <Button
-            variant='light'
-            size='sm'
-            className='rounded-full text-gray-400 hover:text-default-foreground'
-          >
-            <EllipsisIcon className='text-inherit' size={18} />
-          </Button>
-        </Tooltip>
+            <Tooltip content='Comment'>
+              <Button
+                variant='light'
+                size='sm'
+                className='rounded-full flex flex-row gap-2 text-gray-400 hover:text-default-foreground'
+                onPress={() => togglePostModal(true)}
+              >
+                <MessageSquareIcon className='text-inherit' size={18} />
+                {post?.reply_count && post?.reply_count > 0 && (
+                  <span className='text-sm'>{post?.reply_count}</span>
+                )}
+              </Button>
+            </Tooltip>
+            <Tooltip content='Clone (repost)'>
+              <Button
+                variant='light'
+                size='sm'
+                className='rounded-full text-gray-400 hover:text-default-foreground'
+              >
+                <Repeat2Icon className='text-inherit' size={22} strokeWidth={1.5} />
+              </Button>
+            </Tooltip>
+            <Tooltip content='Backup (Bookmark)'>
+              <Button
+                variant='light'
+                size='sm'
+                className='rounded-full text-gray-400 hover:text-default-foreground'
+              >
+                <ArchiveIcon className='text-inherit' size={18} />
+              </Button>
+            </Tooltip>
+            <Tooltip content='More'>
+              <Button
+                variant='light'
+                size='sm'
+                className='rounded-full text-gray-400 hover:text-default-foreground'
+              >
+                <EllipsisIcon className='text-inherit' size={18} />
+              </Button>
+            </Tooltip>
+          </>
+        )}
       </CardFooter>
     </>
   );
